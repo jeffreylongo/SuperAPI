a2cf94b6eab2ae41a209f4af465fa83cb2ae7548
Jeff Longo <jeffreylongo@yahoo.com>
2017-08-08 13:17:32 -0400

    AP services, controller and view added and working to hit API

---
+++ SuperAPI/SuperAPI/Services/ApApiServices.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+using SuperAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace SuperAPI.Services
+{
+    public class ApApiServices
+    {
+        public static IEnumerable<APStoryModel> GetAPNews()
+        {
+            var url = $"https://newsapi.org/v1/articles?source=associated-press&sortBy=top&apiKey=1031788725c8436e9b0dfd92c9995249";
+            var request = WebRequest.Create(url);
+            var response = request.GetResponse();
+            var rawResponse = String.Empty;
+            using (var reader = new StreamReader(response.GetResponseStream()))
+            {
+                rawResponse = reader.ReadToEnd();
+            }
+
+            JObject json = JObject.Parse(rawResponse);
+
+            var ApStories = json["articles"]
+                .Select(s => new APStoryModel
+                {
+                    Author = s["author"].ToString(),
+                    Title = s["title"].ToString(),
+                    Url = s["url"].ToString(),
+                    ImgUrl = s["urlToImage"].ToString()
+                })
+                .GroupBy(g => g.Url).Select(s => s.First());
+
+            return ApStories;
+            
+        }
+    }
+}
