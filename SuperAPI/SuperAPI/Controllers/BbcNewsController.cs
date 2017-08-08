580600f895a3f6102b5cbdae3c8ceb361db50ed2
Jeff Longo <jeffreylongo@yahoo.com>
2017-08-08 12:47:12 -0400

    BBC news api services and controller and view built, tested and functional.

---
+++ SuperAPI/SuperAPI/Controllers/BbcNewsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperAPI.Controllers
+{
+    public class BbcNewsController : Controller
+    {
+        // GET: BbcNews
+        public ActionResult Index()
+        {
+            var bbcNews = Services.BBCApiServices.GetBBCNews();
+
+            return View(bbcNews);
+        }
+    }
+}
---
+++ SuperAPI/SuperAPI/Services/BBCApiServices.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using SuperAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace SuperAPI.Services
+{
+    public class BBCApiServices
+    {
+        public static IEnumerable<BBCStoryModel> GetBBCNews()
+        {
+            var url = $"https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=1031788725c8436e9b0dfd92c9995249";
+            var request = WebRequest.Create(url);
+            var response = request.GetResponse();
+            var rawResponse = String.Empty;
+            using (var reader = new StreamReader(response.GetResponseStream()))
+            {
+                rawResponse = reader.ReadToEnd();
+            }
+
+            JObject json = JObject.Parse(rawResponse);
+
+            var bbcStories = json["articles"]
+                .Select(s => new BBCStoryModel
+                {
+                    Author = s["author"].ToString(),
+                    Title = s["title"].ToString(),
+                    Url = s["url"].ToString(),
+                    ImgUrl = s["urlToImage"].ToString()
+                }).GroupBy(g => g.Url).Select(s => s.First());
+
+            return bbcStories;
+        }
+    }
+}
