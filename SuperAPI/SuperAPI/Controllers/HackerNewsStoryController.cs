ab94d2fb8d0112c655845bb2a59ed38b9cdce8ca
Jeff Longo <jeffreylongo@yahoo.com>
2017-08-08 12:00:33 -0400

    Hacker News Tab working

---
+++ SuperAPI/SuperAPI/Controllers/HackerNewsStoryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperAPI.Controllers
+{
+    public class HackerNewsStoryController : Controller
+    {
+        // GET: HackerNewsStory
+        public ActionResult Index()
+        {
+            var hackerNewsStories = Services.HackerNewsAPIServices.GetHackerNews();
+
+            return View(hackerNewsStories);
+        }
+    }
+}
