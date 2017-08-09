b8440af93f0af72cbf6c4b6389b1eb06915fd615
Jeff Longo <jeffreylongo@yahoo.com>
2017-08-09 13:23:14 -0400

    added IGN services, controller and view. working. changed table layout some for view.

---
+++ SuperAPI/SuperAPI/Controllers/IgnNewsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperAPI.Controllers
+{
+    public class IgnNewsController : Controller
+    {
+        // GET: IgnNews
+        public ActionResult Index()
+        {
+            var ignStories = Services.IgnApiServices.GetIgnNews();
+
+            return View(ignStories);
+        }
+    }
+}
---
+++ SuperAPI/SuperAPI/Models/IgnNewsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperAPI.Models
+{
+    public class IgnNewsModel
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string ImgUrl { get; set; }
+    }
+}
